a61c14e8e988650806ae49296cf3b733fc5a2ec0
r0mka <romanliveshere@gmail.com>
2020-08-30 13:32:46 -0700

    Create basic auth posts profileroutes. Create User model

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +7,9 @@ connectDB();
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.get('/', (req, res) => res.send('API running'));
 
