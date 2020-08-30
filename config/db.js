1120446c7fa4597cdd07463d8469197bf2558965
r0mka <romanliveshere@gmail.com>
2020-08-30 12:49:04 -0700

    Setup mongo and mongoose

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+// brings the variables from default.json
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MONGO DB CONNECTED');
+  } catch (err) {
+    console.log('DB CONNECTION ERROR');
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
