d0eb82f7369d797c32ee505df7d3f9659c979d0c
r0mka <romanliveshere@gmail.com>
2020-10-07 21:40:29 -0700

    some change

---
+++ client/src/actions/auth.js
@@ -0,0 +1,33 @@
+import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import { setAlert } from './alert';
+import axios from 'axios';
+
+// Register User
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post('/api/users/', body, config);
+
+    console.log('HELLO');
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    console.log(err);
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setformData] = useState({
     name: '',
     email: '',
@@ -25,7 +26,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords don not match', 'danger');
     } else {
-      console.log('SUCCESS');
+      register({ name, email, password });
     }
   };
   return (
@@ -46,17 +47,17 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={handleChange}
-            required
+            // required
           />
         </div>
         <div className="form-group">
           <input
-            type="email"
+            // type="email"
             placeholder="Email Address"
             name="email"
             value={email}
             onChange={handleChange}
-            required
+            // required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -68,7 +69,7 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
+            // minLength="6"
             value={password}
             onChange={handleChange}
           />
@@ -78,7 +79,7 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
+            // minLength="6"
             value={password2}
             onChange={handleChange}
           />
@@ -94,5 +95,6 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
